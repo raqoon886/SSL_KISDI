ca9e328089d88449e17ef9e40dd7ec99dc78be3f
raqoon886 <wieder886@gmail.com>
2022-10-08 22:29:35 +0900

    add tokenizer path to tokenize inputs in the datasets class

--- topic_transformer/datasets/SentenceLabelDataset.py
+++ topic_transformer/datasets/SentenceLabelDataset.py
@@ -2,22 +2,26 @@ import torch
 from torch.utils.data import Dataset
 from typing import List, Tuple
 from gensim.models.ldamodel import LdaModel
+from konlpy.tag import Mecab
 
 
 class SentenceLabelDataset(Dataset):
     def __init__(self,
+                 dic_path: str,
                  train_docs: List[str],
-                 corpus: List[List[Tuple]],
                  lda_model: LdaModel,
                  num_topics: int = 20
                  ):
 
+        mecab = Mecab(dic_path)
         self.train_docs = train_docs
         self.num_topics = num_topics
 
         self.labels = []
-        for doc in range(len(train_docs)):
-            probabilities = [b for (a, b) in lda_model.get_document_topics(corpus[doc], minimum_probability=1e-5)]
+        for doc in train_docs:
+            nouns = [n for n in mecab.nouns(doc) if len(n) > 1]
+            content = lda_model.id2word.doc2bow(nouns)
+            probabilities = [b for (a, b) in lda_model.get_document_topics(content, minimum_probability=1e-5)]
             if len(probabilities) < self.num_topics:
                 continue
             self.labels.append(probabilities)
